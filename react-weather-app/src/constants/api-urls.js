6ecc8376c376f2452f58a56deb07265752db9f4d
Franco Alejandro Garcia <francoagarcia@gmail.com>
2020-03-11 13:24:11 -0300

    Update api-urls.js

--- react-weather-app/src/constants/api-urls.js
+++ react-weather-app/src/constants/api-urls.js
@@ -1,2 +1,2 @@
-export const apiKey = "5a647923bfbe2ecc59ca877bc539241e";
+export const apiKey = ""; //https://home.openweathermap.org/api_keys
 export const baseURL = "https://api.openweathermap.org/data/2.5/weather";
